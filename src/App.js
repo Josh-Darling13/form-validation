22860c75646976cbb0d3fabc92b5580a2cc463af
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-18 07:31:56 -0400

    added html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './Main';
+import boostrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+
 export default function Main() {
   return (
     <div>
-       <h1> Main </h1>
+       <div className="row justify-content-center">
+
+            <div className='col-md-6'>
+                <h1>Form Validation</h1>
+
+                <input type="text" placeholder='username' className='form-control'></input>
+                <input type="text" placeholder='email' className='form-control'></input>
+                <input type="text" placeholder='password' className='form-control'></input>
+                <input type="text" placeholder='confirm password' className='form-control'></input>
+
+                <button className='btn btn-success'>Submit</button>
+
+            </div>
+
+       </div>
     </div>
   )
 }
