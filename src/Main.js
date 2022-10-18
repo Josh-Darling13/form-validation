cbbff3ef3f2859aaca5c01cce44242b98b80fab7
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-18 16:39:08 -0400

    completed syling

--- src/Main.js
+++ src/Main.js
@@ -5,19 +5,14 @@ export default function Main() {
   return (
     <div>
        <div className="row justify-content-center">
-
-            <div className='col-md-6'>
+            <div className='col-md-5 shadow-lg p-3 mb-5 bg-white rounded'>
                 <h1>Form Validation</h1>
-
                 <input type="text" placeholder='username' className='form-control'></input>
                 <input type="text" placeholder='email' className='form-control'></input>
                 <input type="text" placeholder='password' className='form-control'></input>
                 <input type="text" placeholder='confirm password' className='form-control'></input>
-
                 <button className='btn btn-success'>Submit</button>
-
             </div>
-
        </div>
     </div>
   )
